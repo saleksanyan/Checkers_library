f9a8b3e0714cf92069684275d4b575078ac0a380
SerobSayadyan <serobsayadyan97@gmail.com>
2024-05-31 17:59:08 +0400

    Add some interface for front and back ends

---
+++ package/src/Validations.ts
@@ -0,0 +1,32 @@
+import Position from "./Position";
+import { BoardConstants, Color } from "./Constants";
+import Board from "./Board";
+import Figure from "./Figure";
+import Move from "./Move";
+
+class Validations {
+	public static isValidPlace(row: number, column: number): boolean {
+		return column < BoardConstants.COLUMNS && row < BoardConstants.ROWS && column >= 0 && row >= 0;
+	}
+
+	public static isValidPosition(userInput: string, board: Board): boolean {
+		if (userInput.length !== 2) return false;
+		if (!this.isNumber(userInput.charAt(1))) return false;
+		let position = new Position(userInput);
+		let figure = board.getBoard()[position.getRow()][position.getColumn()];
+		let hasSameColor = false;
+		if (figure instanceof Figure) {
+			hasSameColor = figure.getColor() === board.getWhosTurn();
+		}
+		return hasSameColor && this.isValidPlace(position.getRow(), position.getColumn());
+	}
+
+	public static placeIsEmpty(row: number, column: number, board: Board): boolean {
+		return board.getBoard()[row][column] === Color.EMPTY_PLACE;
+	}
+	public static isNumber(value: string): boolean {
+		return value !== "" && !isNaN(Number(value.toString()));
+	}
+}
+
+export default Validations;
